a703959d8f744366a3c3cf5c97aeb6cb214a941d
Rubens Rafael <70234898+RubensRafael@users.noreply.github.com>
2025-01-09 15:20:37 -0300

    PR to merge translations from BUKY-208 branch (#66)

--- dev.js
+++ dev.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-restricted-imports */
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { readAndMergeJson } from "./functions.js"
-import input from "./input.json" assert { type: "json" }
+import input from "./input.json" with { type: "json" }
 
 readAndMergeJson("input.json", "./messages/en.json", false)
